7e34f08ef85d9018f71c0496446be287f25d7266
Mihailo34 <mihailoplavsic34@gmail.com>
2022-03-29 03:11:01 +0200

    Zadatak03

--- EMC- AWAS/Vezbe05/Fajlovi/Zadatak03/Zadatak03.py
+++ EMC- AWAS/Vezbe05/Fajlovi/Zadatak03/Zadatak03.py
@@ -1,3 +1,11 @@
 # %%
 import math
-
+f_max=2.7e9
+c=3e8
+print('Sprega je veca u slucaju pod b)')
+print('Sprega je maksimalna na ucestanosit: 2.7GHz')
+L=2*(40+20)*1e-3
+Lambda=c/f_max
+print('Odnos talasne duzine u slucaju maksimalne sprege i duzine spoljasnje konture je: ', str(Lambda/L),
+ 'sto je uporedivo obimom zatvorene zicane kruzne antene')
+# %%
