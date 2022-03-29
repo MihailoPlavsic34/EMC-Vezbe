82680e89029de10b468343ddd5fbfc3288f5ad2f
Mihailo34 <mihailoplavsic34@gmail.com>
2022-03-29 05:12:48 +0200

    Predat

--- EMC- AWAS/Vezbe05/Fajlovi/Zadatak01/Zadatak01.py
+++ EMC- AWAS/Vezbe05/Fajlovi/Zadatak01/Zadatak01.py
@@ -8,3 +8,5 @@ r=h/50
 s1 = math.log10(1.58/0.6)/math.log10(14e-3/10e-3)
 s2 = math.log10(168/140)/math.log10(6/5)
 print('Za d=(10,30)mm polje opada sa stepenom: ', s1, 'Za d=(5,10)m polje opada sa stepenom: ', s2)
+
+# %%
